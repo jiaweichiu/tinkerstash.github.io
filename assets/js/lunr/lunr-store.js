e9f74698ee6cf4bdb1a4285f3acd28dd96e38482
Jiawei Chiu <jwchiu1@gmail.com>
2018-02-10 14:42:12 -0800

    commit

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Hello world",
+        "excerpt":"Some testingHello world!Testing mathjax:How this is builtWe fork the Minimal Mistakes repo and use it to generate static content into a Github Pages repo. We do not need Github Pages support for Jekyll.Remember to add the following to _includes/head/custom.html for the LaTeX to work:&lt;script type=\"text/javascript\" async  src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML\"&gt;&lt;/script&gt;","categories": ["miscel"],
+        "tags": [],
+        "url": "/miscel/hello-world/",
+        "teaser":null}]
