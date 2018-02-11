85afbb7de567e91e3d5505f33e2c0e3ec0e94ea9
Jiawei Chiu <jwchiu1@gmail.com>
2018-02-10 17:33:46 -0800

    commit

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "Feature engineering tricks",
+        "excerpt":"StandardizationSome algorithms are quite sensitive to the scale of each column or feature, e.g., SVM, neural nets. Here are some commons of preprocessing: Standard scaler: where is the mean and is the standard deviation. See sklearn.preprocessing.StandardScaler. If your data is not Gaussian-like, this might turn out badly. See below for...","categories": ["machine-learning"],
+        "tags": [],
+        "url": "/machine-learning/feature-engineering-tricks/",
+        "teaser":null},{
         "title": "Hello world",
         "excerpt":"Some testingHello world!Testing mathjax:How this is builtWe fork the Minimal Mistakes repo and use it to generate static content into a Github Pages repo. We do not need Github Pages support for Jekyll.Remember to add the following to _includes/head/custom.html for the LaTeX to work:&lt;script type=\"text/javascript\" async  src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML\"&gt;&lt;/script&gt;","categories": ["miscel"],
         "tags": [],
